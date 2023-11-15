5d3836837884029019eb0b3a7fa2c4edaa6cc658
Kris706f <126452879+Kris706f@users.noreply.github.com>
2023-11-15 15:30:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -23,4 +23,26 @@ function checkVisibility(entries, observer) {
 
 
 
+
+
+
+  // Funktion for at kontrollere synligheden af sektioner
+function checkVisibility(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+      } else {
+        entry.target.classList.remove('in-view');
+      }
+    });
+  }
   
+ 
+  
+  // Hent alle sektioner med tekstelementer (h2)
+  const textSections = document.querySelectorAll('h2');
+  
+  // Tilføj hver tekstsektion til observeren
+  textSections.forEach((section) => {
+    observer.observe(section);
+  });
